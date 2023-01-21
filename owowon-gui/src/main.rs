19d70fe007ead73c7cc8f70cf330c54dbc2e6261
Dennis Duda <git@seri.tools>
2023-01-21 21:09:29 +0100

    fix: hide console window

--- owowon-gui/src/main.rs
+++ owowon-gui/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(array_chunks)]
 #![warn(clippy::all, rust_2018_idioms)]
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 mod app;
 mod device_run;
